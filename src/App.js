0fa2b9fd62d3cbfb5d011ece46bf2d5e54c35c41
saltanatbe <wthigos@gmail.com>
2022-06-14 01:03:12 +0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import './responsive.css';
+import image1 from './background_images/image1.jpg'
+import image2 from './background_images/image2.jpg'
+import image3 from './background_images/image3.jpg'
+import image4 from './background_images/image4.jpg'
+import image5 from './background_images/image5.jpg'
+import React from "react";
+import Header from "./components/header"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-wrapper">
+      <Header/>
+      <div className="main" >
+        <div className="image-wrapper" style={{ backgroundImage: "url("+image1+")" }}>
+          <p className="txt">Hey</p>
+        </div>
+        <div className="image-wrapper" style={{ backgroundImage: "url("+image2+")" }}>
+          <p className="txt"> Let's</p>
+        </div>
+        <div className="image-wrapper" style={{ backgroundImage: "url("+image3+")" }}>
+          <p className="txt">Give</p>
+        </div>
+        <div className="image-wrapper" style={{ backgroundImage: "url("+image4+")" }}>
+          <p className="txt">All</p>
+        </div>
+        <div className="image-wrapper" style={{ backgroundImage: "url("+image5+")" }}>
+          <p className="txt">You can</p>
+        </div>
+    </div>  
     </div>
+   
+      
   );
 }
 
---
+++ src/components/header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./styles.css"
+
+const Header = () =>{
+    return (
+        <div className ="header-wrapper">
+            <div className="header-row">
+                <img src="https://uploads-ssl.webflow.com/5ed0f381d2b21c219f260b60/5ee31dc8601c7f4fb62cb4c1_n17r-logo-red.png" loading="lazy" width="82" height="82" srcset="https://uploads-ssl.webflow.com/5ed0f381d2b21c219f260b60/5ee31dc8601c7f4fb62cb4c1_n17r-logo-red-p-500.png 500w, https://uploads-ssl.webflow.com/5ed0f381d2b21c219f260b60/5ee31dc8601c7f4fb62cb4c1_n17r-logo-red-p-800.png 800w, https://uploads-ssl.webflow.com/5ed0f381d2b21c219f260b60/5ee31dc8601c7f4fb62cb4c1_n17r-logo-red-p-1080.png 1080w, https://uploads-ssl.webflow.com/5ed0f381d2b21c219f260b60/5ee31dc8601c7f4fb62cb4c1_n17r-logo-red.png 1136w" sizes="(max-width: 479px) 35vw, 100px" alt="" class="image-19"/>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Header;
